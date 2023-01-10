895c2feba2451bf98819f21fac606f0f5346588b
michel-lp1 <michel.lp@outlook.com>
2023-01-09 21:50:41 -0300

    inserindo logo e nome

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './componentes/Logo'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
+    <div className='App'>
+      <header className='App-header'>
+        <Logo></Logo>    
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
