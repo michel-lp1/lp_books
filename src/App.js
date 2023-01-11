16594ee1a7142a769ac8bd110b50d1779a1396a8
michel-lp1 <michel.lp@outlook.com>
2023-01-10 22:05:47 -0300

    correções

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Logo from './componentes/Logo'
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <header className='App-header'>
-        <Logo></Logo>    
+        <Logo></Logo>
       </header>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/componentes/Logo/Index.js
+++ src/componentes/Logo/Index.js
@@ -1,11 +1,16 @@
+ 
 import logo from '../../imagens/logo.svg'
 
 function Logo() {
     return (
-    <div className='logo'>
-      <img src={logo} alt="logo"></img>
-    <p>LP Books</p>
-    </div>
+        <div className='logo'>
+            <img
+                src={logo}
+                alt='logo' 
+                className='logo-img'
+            ></img>
+            <p><strong>Alura</strong>Books</p>
+       </div>
     )
 }
 
